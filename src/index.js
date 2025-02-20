8bf2ff0fbaf8381fb9886346c05688949116c656
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-02-20 21:16:09 +0300

    Implement condition check for img render.

--- src/index.js
+++ src/index.js
@@ -4,49 +4,78 @@ import * as icon from "./img/images";
 const API_KEY = "V95FANYRGWCKFGZNYXGJFVZJE";
 
 const mainBlock = document.querySelector(".main-block");
-const rainImg = document.createElement("img");
-rainImg.src = icon.foggy;
-
-mainBlock.append(rainImg);
 
 async function callAPI(city, callback) {
-  const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${API_KEY}`,
-    { mode: `cors` }
-  );
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${API_KEY}`,
+      { mode: `cors` }
+    );
 
-  const weatherData = await response.json();
-  callback(weatherData);
-  return weatherData;
+    const weatherData = await response.json();
+    callback(weatherData);
+    return weatherData;
+  } catch (error) {
+    const errorLog = document.createElement("p");
+    errorLog.textContent = "City was not found, please try again.";
+    mainBlock.append(errorLog);
+  }
 }
 
 function logWeather(data) {
   console.log(data);
 }
 
-// write a separate weather check
+// write a separate weather check. Pass response.currentConditions.conditions
+function conditionCheck(data) {
+  const lowercase = data.toLowerCase();
+  if (lowercase.includes("cloud") || lowercase.includes("overcast")) {
+    return icon.cloud;
+  } else if (lowercase.includes("rain")) {
+    return icon.rainy;
+  } else if (lowercase.includes("snow")) {
+    return icon.snowy;
+  } else if (lowercase.includes("clear")) {
+    return icon.partly_cloudy;
+  } else if (lowercase.includes("fog")) {
+    return icon.foggy;
+  } else {
+    return icon.thunderstorm;
+  }
+}
 
 const renderDOM = (function () {
   const header = (responseJSON) => {
     const head = document.createElement("h1");
     head.textContent = responseJSON.resolvedAddress;
 
-    mainBlock.append(head);
+    return head;
+  };
+  const image = (responseJSON) => {
+    const imageWeather = document.createElement("img");
+    imageWeather.src = conditionCheck(
+      responseJSON.currentConditions.conditions
+    );
+    return imageWeather;
   };
 
-  return { header };
+  const conditions = (responseJSON) => {
+    const conditionBlock = document.querySelector(".conditions");
+  };
+  return { header, image };
 })();
 
-async function render() {
-  const data = await callAPI("London", logWeather);
-  renderDOM.header(data);
+async function render(cityName) {
+  const data = await callAPI(cityName, logWeather);
+
+  mainBlock.append(renderDOM.header(data), renderDOM.image(data));
 }
 
-render(callAPI("London", logWeather));
+render("kyiv");
 
 const submit = document.querySelector("#submit");
 
 submit.addEventListener("click", (event) => {
   event.preventDefault();
-  
+  const search = document.querySelector("#search");
 });
