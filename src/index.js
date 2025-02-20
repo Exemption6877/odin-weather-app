e89aea207fca83cc38975ee04c6163a27d2dc809
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-02-20 20:33:55 +0300

    Test JSON output, event listener submit.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import * as icon from "./img/images";
 const API_KEY = "V95FANYRGWCKFGZNYXGJFVZJE";
 
 const mainBlock = document.querySelector(".main-block");
-
 const rainImg = document.createElement("img");
 rainImg.src = icon.foggy;
 
@@ -22,7 +21,32 @@ async function callAPI(city, callback) {
 }
 
 function logWeather(data) {
-  return console.log(data);
+  console.log(data);
 }
 
-callAPI("London", logWeather);
+// write a separate weather check
+
+const renderDOM = (function () {
+  const header = (responseJSON) => {
+    const head = document.createElement("h1");
+    head.textContent = responseJSON.resolvedAddress;
+
+    mainBlock.append(head);
+  };
+
+  return { header };
+})();
+
+async function render() {
+  const data = await callAPI("London", logWeather);
+  renderDOM.header(data);
+}
+
+render(callAPI("London", logWeather));
+
+const submit = document.querySelector("#submit");
+
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
+  
+});
