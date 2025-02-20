ab2e79a4e4f84b475044c37567f14c728271a8ca
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-02-20 06:48:40 +0300

    Add images, create containers for API output

---
+++ src/img/images.js
@@ -0,0 +1,9 @@
+import bedtime from "./bedtime.svg";
+import cloud from "./cloud.svg";
+import foggy from "./foggy.svg";
+import partly_cloudy from "./partly_cloudy.svg";
+import rainy from "./rainy.svg";
+import snowy from "./snowy.svg";
+import thunderstorm from "./thunderstorm.svg";
+
+export { bedtime, cloud, foggy, partly_cloudy, rainy, snowy, thunderstorm };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import "./styles.css";
+import * as icon from "./img/images";
+
+const mainBlock = document.querySelector(".main-block");
+
+const rainImg = document.createElement("img");
+rainImg.src = icon.foggy;
+
+mainBlock.append(rainImg);
