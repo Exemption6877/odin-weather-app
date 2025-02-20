fd398ccffc414c194bdb6ed8edcf57db52902fc6
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-02-20 07:17:14 +0300

    Query API response.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import "./styles.css";
 import * as icon from "./img/images";
 
+const API_KEY = "V95FANYRGWCKFGZNYXGJFVZJE";
+
 const mainBlock = document.querySelector(".main-block");
 
 const rainImg = document.createElement("img");
 rainImg.src = icon.foggy;
 
 mainBlock.append(rainImg);
+
+async function callAPI(city, callback) {
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${API_KEY}`,
+    { mode: `cors` }
+  );
+
+  const weatherData = await response.json();
+  console.log(weatherData);
+  return weatherData;
+}
+
+callAPI("London");
